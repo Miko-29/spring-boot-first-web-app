4287756c09c825c10460feb6e14388ae20588de8
Miko-29 <muskan2912002@gmail.com>
2026-07-09 22:51:51 +0530

    hardcoded validation

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	private AuthenticationService authenticationService;
+
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String goToLoginPage() {
 		return "login";
@@ -16,8 +23,20 @@ public class LoginController {
 
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		model.put("name", name);
-		model.put("password", password);
-		return "welcome";
+
+		if (authenticationService.authenticate(name, password)) {
+			model.put("name", name);
+			model.put("password", password);
+
+			// Authentication
+			// name - in28minutes
+			// password - dummy
+
+			return "welcome";
+		}
+
+		model.put("errorMessage", "Invalid Credentials! Please try again.");
+		return "login";
 	}
+
 }
